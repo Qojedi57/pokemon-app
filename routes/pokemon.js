3b2190d883b7decb09e1247eadad969dfa0fea73
Qojedi57 <111308861+Qojedi57@users.noreply.github.com>
2023-02-07 19:43:13 -0800

    Express pokemon app

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -4,7 +4,7 @@ export default function setupPokemonRouter(db) {
   const router = express.Router();
 
   //Create our GET route that just sends back the pokemon data
-db.json  router.get("/", function (_request, response) {
+  router.get("/", function (_request, response) {
     //The underscore means to ignore the param that's not being used
     response.status(200).json({
       //Set our response to have a status of 200 (OK!) and to respond with JSON
@@ -14,16 +14,65 @@ db.json  router.get("/", function (_request, response) {
   });
 
   router.get("/:pokemon", function (request, response) {
-    const todo = request.params.todo;
+    const pokemon = request.params.pokemon;
 
-    const currentTodo = db.data.pokemon.find(
-      (todoItem) => todoItem.id === todo
+    const currentpokemon = db.data.pokemon.find(
+      (pokemonItem) => pokemonItem.name === pokemon
     );
 
     response.status(200).json({
       success: true,
-      todo: currentTodo,
+      name: currentpokemon,
     });
   });
+
+  router.post("/", function (request, response) {
+    //Push the new todo
+    console.log(request.body)
+    db.data.pokemon.push({
+      name: request.body.name
+    });
+
+    //Save the pokemon to the "database"
+    db.write();
+
+    //Respond with 200 (OK!) and tell the user the request is successful
+    response.status(200).json({
+      success: true,
+    });
+  });
+
+  router.put("/:pokemon", function (request, response) {
+    const pokemon = request.params.pokemon;
+    console.log(pokemon);
+
+    const pokemonIndex = db.data.pokemon.findIndex(
+      (currentpokemon) => currentpokemon.id === pokemon
+    );
+
+    db.data.pokemon[pokemonIndex].name = request.body.name;
+
+    db.write();
+
+    response.status(200).json({
+      success: true,
+    });
+  });
+
+  router.delete("/:pokemon", (request, response) => {
+    const pokemon = request.params.pokemon;
+    console.log(pokemon);
+
+    const pokemonIndex = db.data.pokemon.findIndex(
+      (currentpokemon) => currentpokemon.id === pokemon
+    );
+    db.data.pokemon.splice(pokemonIndex, 1);
+    db.write();
+
+    response.status(200).json({
+      success: true,
+    });
+  });
+
   return router;
 }
